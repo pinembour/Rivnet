030a4d2ee727d344aef372b8d970a3d55d1d0dde
gnikwo <gnikwo@hotmail.com>
2017-08-29 11:12:08 +0200

    Synch

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*
+#
+
+from django.conf.urls import url
+
+from core import views
+
+urlpatterns = [
+    url(r'^synchronize$', views.sync, name='synchronize'),
+]
+
