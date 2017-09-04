84f6a7636754d083ae33b0d34e29dbd8c478850b
gnikwo <gnikwo@hotmail.com>
2017-09-04 13:26:51 +0200

    optimizer basis

---
+++ optimizer/urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*
+#
+
+from django.conf.urls import url
+
+from optimizer import views
+
+urlpatterns = [
+    url(r'^optimize$', views.optimize, name='optimize'),
+]
+
